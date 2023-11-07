0235513c98c015a13cf5a23a2cae431fba1b4548
haiibo <boyhaibo@gmail.com>
2023-11-07 15:34:47 +0800

    add armv8 openwrt compile

--- diy-mini.sh
+++ diy-mini.sh
@@ -24,7 +24,7 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/lu
 
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
 git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
@@ -101,9 +101,6 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 
 # 取消对 samba4 的菜单调整
 # sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
-
-# 修改插件名字
-# sed -i 's/"带宽监控"/"监控"/g' `grep "带宽监控" -rl ./`
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
